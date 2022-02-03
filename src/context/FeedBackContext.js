858b90e1cdc20c69acfbc1fcde70c44a6039ad58
igoryashkka <83956200+igoryashkka@users.noreply.github.com>
2022-02-03 15:40:07 +0200

    Update FeedBackContext.js

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -7,25 +7,11 @@ export const FeedBackProvider = ({ children }) => {
   const [FeedBack, setFeedBack] = useState([
     {
       id: 2,
-      rating: 5,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      rating: 10,
+      text: 'Самая лучшая в мире зайка!!!',
     },
-    {
-      id: 3,
-      rating: 8,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: 2,
-      rating: 5,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: 2,
-      rating: 5,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    
+
+ 
   ]);
 
   const [FeedBackEdit, setFeedBackEdit] = useState(
